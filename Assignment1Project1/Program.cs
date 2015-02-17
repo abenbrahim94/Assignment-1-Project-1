98d7b5b958c52729b16c5089551d466d59464cbc
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-17 10:48:10 -0500

    FinalCommit

---
+++ Assignment1Project1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Welcome to Program1\"");
+            Console.WriteLine();
+            Console.Write("Type any character: ");
+            string prompt = Console.ReadLine();
+            char newValue = Convert.ToChar(prompt);
+            Console.WriteLine("Your Response is: \"{0}\"", newValue);
+            Console.WriteLine();
+            Console.WriteLine("\"Press any key to exit\"");
+            Console.ReadKey();
+        }
+    }
+}
